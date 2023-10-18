d356160c3d4741248d2dafdb86d62e9beeeb9a17
Slime <slime@daum.net>
2023-10-17 22:32:55 -0400

    Add codes for the migrations

--- orm.config.ts
+++ orm.config.ts
@@ -1,11 +1,13 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { DataSource } from "typeorm";
 
 function ormConfig(): TypeOrmModuleOptions {
     const commonConf = {
         SYNCHRONIZE: false,
         ENTITIES: [__dirname + '/**/*.entity{.ts,.js}'],
-        MIGRATIONS: [__dirname + '/migrations/**/*{.ts,.js}'],
+        MIGRATIONS: [__dirname + '/**/migrations/*{.ts,.js}'],
         MIGRATIONS_RUN: false,
+        MIGRATIONS_TABLE_NAME: 'migrations',
     };
 
     return {
@@ -21,7 +23,21 @@ function ormConfig(): TypeOrmModuleOptions {
         entities: commonConf.ENTITIES,
         migrations: commonConf.MIGRATIONS,
         migrationsRun: commonConf.MIGRATIONS_RUN,
+        migrationsTableName: commonConf.MIGRATIONS_TABLE_NAME,
     };
 }
 
 export { ormConfig };
+
+export const AppDataSource = new DataSource({
+      type: 'mysql',
+  database: 'test',
+  host: 'localhost',
+  port: Number(3306),
+  username: 'root',
+  password: '12345',
+  synchronize: false,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/**/migrations/*.ts'],
+  migrationsTableName: 'migrations',
+});
